d21fd9891a6db0bbdde7fa76ba615440006b659c
Pablo Cantero <pablohstc@gmail.com>
2018-10-21 18:17:34 -0400

    Auto merge if :white_check_mark: :shipit:

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
 // 'Application' is defined but never used
-import { Application } from 'probot' // eslint-disable-line
-// import { mergeWhenGreen } from './checkRunHandler'
+import { Application } from "probot"; // eslint-disable-line
 
 export = (app: Application) => {
-  app.on('check_run.completed', async context => {})
-}
+  app.on('check_run.completed', async context => {
+    const github = context.github
+    const checkRun = context.payload.check_run
+    const owner = context.payload.repository.owner.name
+    const repo = context.payload.repository.name
+
+    checkRun.pull_requests.forEach(async pr => {
+      if (!pr.body.replace(/\s/g, '').includes(':white_check_mark: :shipit:')) {
+        return false
+      }
+
+      const checkRuns = (await github.checks.listForRef({
+        owner,
+        repo,
+        ref: pr.head.ref
+      })).data
+
+      const unsuccessful = checkRuns.find(checkRun => {
+        return (
+          checkRun.status !== 'completed' &&
+          checkRun.conclusion !== 'success' &&
+          checkRun.app.owner.login === 'travis-ci'
+        )
+      })
+
+      if (!unsuccessful) {
+        const result = await github.pullRequests.merge({
+          owner,
+          repo,
+          number: pr.number
+        })
+
+        if (result.merged) {
+          await github.gitdata.deleteReference({
+            owner,
+            repo,
+            ref: `heads/${pr.head.ref}`
+          })
+        }
+      }
+    })
+  })
+};
