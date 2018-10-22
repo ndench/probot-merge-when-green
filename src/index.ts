76edfd8c38e15e529a8305c1fbd33830787cbe6f
Pablo Cantero <pablohstc@gmail.com>
2018-10-21 20:03:15 -0400

    Add label "merge when green" during the app installation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@
 import { Application } from 'probot' // eslint-disable-line
 
 export = (app: Application) => {
+  app.on('installation_repositories.added', async context => {
+    const github = context.github
+    const owner = context.payload.repository.owner.login
+    const repo = context.payload.repository.name
+    const name = 'merge when green'
+    const description = 'See: https://github.com/phstc/probot-merge-when-green/'
+    const color = '#2cbe4e'
+
+    await github.issues.createLabel({ owner, repo, name, color, description })
+  })
+
   app.on('check_run.completed', async context => {
     const github = context.github
     const checkRun = context.payload.check_run
@@ -9,13 +20,19 @@ export = (app: Application) => {
     const repo = context.payload.repository.name
 
     checkRun.pull_requests.forEach(async (prRef: any) => {
-      const pr = (await github.pullRequests.get({owner, repo, number: prRef.number})).data
+      const pr = (await github.pullRequests.get({
+        owner,
+        repo,
+        number: prRef.number
+      })).data
 
       const shouldMerge = pr.labels.find(label => {
         return label.name === 'merge when green'
       })
 
-      if(!shouldMerge) { return }
+      if (!shouldMerge) {
+        return
+      }
 
       const checks = (await github.checks.listForRef({
         owner,
