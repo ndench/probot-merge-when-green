13fba629ce2f13fe3aacbf2fe6b9addedb505050
Pablo Cantero <pablohstc@gmail.com>
2018-11-18 19:15:26 -0500

    Extract logic to checkRunCompletedHandler

---
+++ src/checkRunCompletedHandler.ts
@@ -0,0 +1,44 @@
+const MERGE_LABEL = 'merge when green'
+const SUPPORTED_CI = ['circleci', 'travis-ci']
+
+async function createLabel(context: any) {
+  const github = context.github
+  const name = 'merge when green'
+  const description = 'See: https://github.com/phstc/probot-merge-when-green/'
+  const color = '#2cbe4e'
+
+  await github.issues.createLabel(context.repo({ name, color, description }))
+}
+
+export async function checkRunCompletedHandler(context: any) {
+  const github = context.github
+  const checkRun = context.payload.check_run
+
+  checkRun.pull_requests.forEach(async (prRef: any) => {
+    const pr = (await github.pullRequests.get(context.repo({ number: prRef.number }))).data
+
+    const shouldMerge = pr.labels.find((label: any) => {
+      return label.name === MERGE_LABEL
+    })
+
+    if (!shouldMerge) {
+      return
+    }
+
+    const checks = (await github.checks.listForRef(context.repo({ ref: pr.head.ref }))).data
+
+    const unsuccessful = checks.check_runs.find((cr: any) => {
+      return (
+        cr.status !== 'completed' || (cr.status === 'completed' && cr.conclusion !== 'success' && SUPPORTED_CI.includes(cr.app.owner.login))
+      )
+    })
+
+    if (!unsuccessful) {
+      const result = await github.pullRequests.merge(context.repo({ number: pr.number }))
+
+      if (result.data.merged) {
+        await github.gitdata.deleteReference(context.repo({ ref: `heads/${pr.head.ref}` }))
+      }
+    }
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,10 @@
 // 'Application' is defined but never used
 import { Application } from 'probot' // eslint-disable-line
+import { checkRunCompletedHandler } from './checkRunCompletedHandler'
+
 
 export = (app: Application) => {
-  app.on('installation_repositories.added', async context => {
-    const github = context.github
-    const owner = context.payload.repository.owner.login
-    const repo = context.payload.repository.name
-    const name = 'merge when green'
-    const description = 'See: https://github.com/phstc/probot-merge-when-green/'
-    const color = '#2cbe4e'
-
-    await github.issues.createLabel({ owner, repo, name, color, description })
-  })
-
   app.on('check_run.completed', async context => {
-    const github = context.github
-    const checkRun = context.payload.check_run
-    const owner = context.payload.repository.owner.login
-    const repo = context.payload.repository.name
-
-    checkRun.pull_requests.forEach(async (prRef: any) => {
-      const pr = (await github.pullRequests.get({
-        owner,
-        repo,
-        number: prRef.number
-      })).data
-
-      const shouldMerge = pr.labels.find(label => {
-        return label.name === 'merge when green'
-      })
-
-      if (!shouldMerge) {
-        return
-      }
-
-      const checks = (await github.checks.listForRef({
-        owner,
-        repo,
-        ref: pr.head.ref
-      })).data
-
-      const unsuccessful = checks.check_runs.find(cr => {
-        return (
-          cr.status !== 'completed' ||
-          (cr.status === 'completed' &&
-          cr.conclusion !== 'success' &&
-          (cr.app.owner.login === 'travis-ci' || cr.app.owner.login === 'circleci'))
-        )
-      })
-
-      if (!unsuccessful) {
-        const result = await github.pullRequests.merge({
-          owner,
-          repo,
-          number: pr.number
-        })
-
-        if (result.data.merged) {
-          await github.gitdata.deleteReference({
-            owner,
-            repo,
-            ref: `heads/${pr.head.ref}`
-          })
-        }
-      }
-    })
+    await checkRunCompletedHandler(context)
   })
 }
