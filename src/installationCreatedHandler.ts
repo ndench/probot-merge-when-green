a2709733c0c5a7723bd7de17ce5a24806dd209fe
Pablo Cantero <pablohstc@gmail.com>
2018-11-18 19:30:06 -0500

    Trying to fix invalid color error

--- src/installationCreatedHandler.ts
+++ src/installationCreatedHandler.ts
@@ -3,7 +3,7 @@ import { MERGE_LABEL } from './checkRunCompletedHandler'
 async function createLabel (context: any, owner: string, repo: string) {
   const github = context.github
   const description = 'See: https://github.com/phstc/probot-merge-when-green/'
-  const color = '#2cbe4e'
+  const color = '2cbe4e'
   await github.issues.createLabel({ owner, repo, name: MERGE_LABEL, color, description })
 }
 
