bf557c46fbea55c4f2a395dfc26c092b16ed4dc3
Pablo Cantero <pablohstc@gmail.com>
2019-06-16 16:49:45 -0400

    Add support for auto closing fixable issues

--- src/checkRunCompletedHandler.ts
+++ src/checkRunCompletedHandler.ts
@@ -1,3 +1,6 @@
+// import { Context } from 'probot'
+import findFixableIssues from './findFixableIssues'
+
 export const MERGE_LABEL = 'merge when green'
 const SUPPORTED_CI = ['circleci', 'travis-ci']
 
@@ -10,37 +13,42 @@ export async function checkRunCompletedHandler (context: any) {
       context.repo({ number: prRef.number })
     )).data
 
-    const shouldMerge = pr.labels.find((label: any) => {
-      return label.name === MERGE_LABEL
-    })
+    const shouldMerge = pr.labels.find((label: any) => label.name === MERGE_LABEL)
 
-    if (!shouldMerge) {
-      return
-    }
+    if (!shouldMerge) return
 
-    const checks = (await github.checks.listForRef(
+    const checks = (await context.github.checks.listForRef(
       context.repo({ ref: pr.head.ref })
     )).data
 
-    const unsuccessful = checks.check_runs.find((cr: any) => {
-      return (
-        cr.status !== 'completed' ||
-        (cr.status === 'completed' &&
-          cr.conclusion !== 'success' &&
-          SUPPORTED_CI.includes(cr.app.owner.login))
-      )
-    })
+    const supportedCheckRuns = checks.check_runs.filter((checkRun: any) => SUPPORTED_CI.includes(checkRun.app.owner.login))
+    if(supportedCheckRuns.length === 0) return
 
-    if (!unsuccessful) {
-      const result = await github.pullRequests.merge(
-        context.repo({ number: pr.number })
-      )
+    const unsuccessfulCheckRun =  checks.check_runs.find((checkRun: any) => checkRun.status !== 'completed' || checkRun.conclusion !== 'success')
+    if(unsuccessfulCheckRun) return
 
-      if (result.data.merged) {
-        await github.gitdata.deleteReference(
-          context.repo({ ref: `heads/${pr.head.ref}` })
-        )
-      }
-    }
+    const result = await context.github.pullRequests.merge(context.repo({ number: pr.number }))
+
+    if (!result.data.merged) return
+
+    await github.gitdata.deleteRef(
+      context.repo({ ref: `heads/${pr.head.ref}` })
+    )
+    // Only close issues if the PR targets the default branch
+    // in order to replicate GitHub's functionality (closing keywords)
+    if (!isTargetDefaultBranch(pr)) return
+
+    await findFixableIssues(pr.body).forEach(async number => closeIssue(context, number))
   })
+}
+
+const isTargetDefaultBranch = (pr: any): boolean => pr.base.repo.default_branch == pr.base.ref
+
+const closeIssue = async (context: any, number: string) => {
+  const github = context.github
+  const issue = (await github.issues.get(context.repo({ number }))).data
+
+  if (issue.pull_request) /* skip pull requests */ return
+
+  await github.issues.edit(context.repo({ state: 'closed', number }))
 }
