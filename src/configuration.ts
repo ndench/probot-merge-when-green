d95c4fbba3853cbabac5eca5bb5e2621be721d6d
Nathan Dench <ndenc2@gmail.com>
2019-07-17 17:52:03 +1000

    Use probot-config to get configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,14 +1,12 @@
 import { Context } from 'probot'
 import { CONFIGURATION_FILE } from './constants'
 
+const getConfig = require('probot-config')
+
 export interface IConfiguration {
   supportedCi: string[];
 }
 
-const defaultConfig: IConfiguration = {
-  supportedCi: ['circleci', 'travis-ci']
-}
-
 export default async function getConfiguration (context: Context): Promise<IConfiguration> {
-  return await context.config(CONFIGURATION_FILE, defaultConfig)
+  return await getConfig(context, CONFIGURATION_FILE)
 }
