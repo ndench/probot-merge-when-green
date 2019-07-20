75ee2d45f5d40ea096cf0ca8c0103184fdc6bf4c
Nathan Dench <ndenc2@gmail.com>
2019-07-20 10:54:00 +1000

    Revert "Remove types to pass CI"
    This reverts commit 157e2e5bf6b92cd52a7ba22b5d65524ec6d8a0d1.

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,12 +1,16 @@
 import { Context } from 'probot' // eslint-disable-line no-unused-vars
 import { CONFIGURATION_FILE } from './constants'
 
-const defaultConfig = {
+export interface IConfiguration {
+  requiredCi: string[];
+}
+
+const defaultConfig: IConfiguration = {
   requiredCi: ['circleci', 'travis-ci']
 }
 
-export default async function getConfiguration (context: Context) {
+export default async function getConfiguration (context: Context): Promise<IConfiguration> {
   // We're typing this explicitly because probot types context.config as possibly returning null
   // but that makes no sense when we're passing in defaultConfig.
-  return context.config(CONFIGURATION_FILE, defaultConfig)
+  return await context.config(CONFIGURATION_FILE, defaultConfig) as IConfiguration
 }
